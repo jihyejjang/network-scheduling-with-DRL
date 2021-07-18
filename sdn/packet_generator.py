0ea6908b4d4c92fb2c69763089af6f0d45fa3e49
류지혜 <jihye@Rjh-MacBook-Air.local>
2021-07-19 04:05:00 +0900

    0718 commit

--- sdn/packet_generator.py
+++ sdn/packet_generator.py
@@ -1,16 +1,16 @@
 from RLswitch import rl_switch
 import time
 from datetime import datetime
-from threading import Timer
-from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER
-from ryu.controller.handler import set_ev_cls
+# from threading import Timer
+# from ryu.controller import ofp_event
+# from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER
+# from ryu.controller.handler import set_ev_cls
 from ryu.lib import hub
-
-from ryu.base import app_manager
-from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
-from ryu.controller.handler import set_ev_cls
+#
+# from ryu.base import app_manager
+# from ryu.controller import ofp_event
+# from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+# from ryu.controller.handler import set_ev_cls
 
 from ryu.lib.packet import packet, ether_types
 from ryu.lib.packet import ethernet
@@ -22,31 +22,10 @@ class FlowGenerator(rl_switch):
 
     def __init__(self, *args, **kwargs):
         super(FlowGenerator, self).__init__(*args, **kwargs)
-        self.cc_thread = None
-        self.dp = {}
-
-    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
-    def _switch_features_handler(self, ev):
-        msg = ev.msg
-        datapath = msg.datapaths
-        self.dp[datapath.id] = datapath
-        self.logger.info("%s초 %0.1f : 스위치 %s 연결" % (
-        (datetime.now() - self.start_time).seconds, (datetime.now() - self.start_time).microseconds / 1000,
-        datapath.id))
-
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-
-        match = parser.OFPMatch()
-        # controller에 전송하고 flow entry modify하는 명령 : 빈 매치를 modify해서 flow-miss를 controller로 보내는 명령
-        actions = [parser.OFPActionOutput(port=ofproto.OFPP_CONTROLLER,
-                                          max_len=ofproto.OFPCML_NO_BUFFER)]
-        self.add_flow(datapath, 0, match, actions, ofproto.OFP_NO_BUFFER)
-
-        if len(self.dp)==6:
-            self.cc_thread = hub.spawn(self._cc_gen1)
+        self.cc_thread = hub.spawn(self._cc_gen1)
 
     def _cc_gen1(self):
+        time.sleep(1)
         datapath = self.dp[1]
         pkt = packet.Packet()
         pkt.add_protocol(ethernet.ethernet(ethertype=ether_types.ETH_TYPE_IEEE802_3,
