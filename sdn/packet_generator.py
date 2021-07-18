c24d56014c3942f696d126c7ca3cc1a570fd1d2c
류지혜 <jihye@Rjh-MacBook-Air.local>
2021-07-19 02:44:27 +0900

    0718 commit

--- sdn/packet_generator.py
+++ sdn/packet_generator.py
@@ -23,6 +23,25 @@ class FlowGenerator(rl_switch):
     def __init__(self, *args, **kwargs):
         super(FlowGenerator, self).__init__(*args, **kwargs)
         self.cc_thread = hub.spawn(self._cc_gen1)
+        self.dp = {}
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def _switch_features_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        self.dp[datapath.id] = datapath
+        self.logger.info("%s초 %0.1f : 스위치 %s 연결" % (
+        (datetime.now() - self.start_time).seconds, (datetime.now() - self.start_time).microseconds / 1000,
+        datapath.id))
+
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        match = parser.OFPMatch()
+        # controller에 전송하고 flow entry modify하는 명령 : 빈 매치를 modify해서 flow-miss를 controller로 보내는 명령
+        actions = [parser.OFPActionOutput(port=ofproto.OFPP_CONTROLLER,
+                                          max_len=ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions, ofproto.OFP_NO_BUFFER)
 
     def _cc_gen1(self):
         datapath = self.dp[1]
