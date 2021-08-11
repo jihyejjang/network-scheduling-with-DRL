544e13da61e9d825390061ec2ee740a5c73d0899
류지혜 <jihye@Rjh-MacBook-Air.local>
2021-08-11 20:51:57 +0900

    0810 commit1

--- sdn/simpleswitch15.py
+++ sdn/simpleswitch15.py
@@ -22,13 +22,28 @@ from ryu.lib.packet import packet, ether_types, in_proto
 from ryu.lib.packet import ethernet,icmp
 from ryu.lib.packet import ether_types
 
+def addr_table():  # address table dictionary is created manually
+    H = ['00:00:00:00:00:0' + str(h) for h in range(1, 9)]  # hosts
+    mac_to_port = {}  # switch는 6개, src는 8개가 존재
+    port = [[1, 2, 3, 3, 3, 3, 3, 3],
+            [3, 3, 1, 2, 3, 3, 3, 3],
+            [1, 1, 2, 2, 3, 3, 3, 3],
+            [1, 1, 1, 1, 2, 2, 3, 3],
+            [1, 1, 1, 1, 2, 3, 1, 1],
+            [1, 1, 1, 1, 1, 1, 2, 3]]  # custom topology에 해당하는 swith,port mapping 정보
+
+    for s in range(1, 7):  # 6 switches
+        mac_to_port.setdefault(s, {})
+        for h in range(len(H)): #0~7
+            mac_to_port[s][H[h]] = port[s - 1][h]
+    return mac_to_port
 
 class SimpleSwitch15(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_5.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch15, self).__init__(*args, **kwargs)
-        self.mac_to_port = {}
+        self.mac_to_port = addr_table()
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -76,18 +91,18 @@ class SimpleSwitch15(app_manager.RyuApp):
         dst = eth.dst
         src = eth.src
         icmp_ = pkt.get_protocol(icmp.icmp)
-        print("icmp",icmp_)
+        if icmp_ != None:
+            print("icmp",icmp_)
 
         dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
-
-        self.logger.info("packet in 스위치%s 출발%s 도착%s %s", dpid, src, dst, in_port)
 
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
 
         if dst in self.mac_to_port[dpid]:
             out_port = self.mac_to_port[dpid][dst]
+            self.logger.info("packet in 스위치%s 출발%s 도착%s %s", dpid, src, dst, in_port)
         else:
             out_port = ofproto.OFPP_FLOOD
 
@@ -107,52 +122,52 @@ class SimpleSwitch15(app_manager.RyuApp):
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
                                   match=match, actions=actions, data=data)
         datapath.send_msg(out)
-
-    #traffic monitoring
-    def _request_stats(self, datapath):
-        self.logger.debug('send stats request: %016x', datapath.id)
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-
-        req = parser.OFPFlowStatsRequest(datapath)
-        datapath.send_msg(req)
-
-        req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
-        datapath.send_msg(req)
-
-    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
-    def _port_stats_reply_handler(self, ev):
-        body = ev.msg.body
-        self.logger.info('port stats : dp %s', ev.msg.datapath.id)
-        self.logger.info('datapath         port     '
-                         'rx-pkts  rx-bytes rx-error '
-                         'tx-pkts  tx-bytes tx-error')
-        self.logger.info('---------------- -------- '
-                         '-------- -------- -------- '
-                         '-------- -------- --------')
-        for stat in sorted(body, key=attrgetter('port_no')):
-            self.logger.info('%016x %8x %8d %8d %8d %8d %8d %8d',
-                             ev.msg.datapath.id, stat.port_no,
-                             stat.rx_packets, stat.rx_bytes, stat.rx_errors,
-                             stat.tx_packets, stat.tx_bytes, stat.tx_errors)
-
-    @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
-    def _flow_stats_reply_handler(self, ev):
-        body = ev.msg.body
-
-        self.logger.info('port stats : dp %s', ev.msg.datapath.id)
-
-        self.logger.info('datapath         '
-                         'in-port  eth-dst           '
-                         'out-port packets  bytes')
-        self.logger.info('---------------- '
-                         '-------- ----------------- '
-                         '-------- -------- --------')
-        for stat in sorted([flow for flow in body if flow.priority == 1],
-                           key=lambda flow: (flow.match['in_port'],
-                                             flow.match['eth_dst'])):
-            self.logger.info('%016x %8x %17s %8x %8d %8d',
-                             ev.msg.datapath.id,
-                             stat.match['in_port'], stat.match['eth_dst'],
-                             stat.instructions[0].actions[0].port,
-                             stat.packet_count, stat.byte_count)
+    #
+    # #traffic monitoring
+    # def _request_stats(self, datapath):
+    #     self.logger.debug('send stats request: %016x', datapath.id)
+    #     ofproto = datapath.ofproto
+    #     parser = datapath.ofproto_parser
+    #
+    #     req = parser.OFPFlowStatsRequest(datapath)
+    #     datapath.send_msg(req)
+    #
+    #     req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
+    #     datapath.send_msg(req)
+    #
+    # @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
+    # def _port_stats_reply_handler(self, ev):
+    #     body = ev.msg.body
+    #     self.logger.info('port stats : dp %s', ev.msg.datapath.id)
+    #     self.logger.info('datapath         port     '
+    #                      'rx-pkts  rx-bytes rx-error '
+    #                      'tx-pkts  tx-bytes tx-error')
+    #     self.logger.info('---------------- -------- '
+    #                      '-------- -------- -------- '
+    #                      '-------- -------- --------')
+    #     for stat in sorted(body, key=attrgetter('port_no')):
+    #         self.logger.info('%016x %8x %8d %8d %8d %8d %8d %8d',
+    #                          ev.msg.datapath.id, stat.port_no,
+    #                          stat.rx_packets, stat.rx_bytes, stat.rx_errors,
+    #                          stat.tx_packets, stat.tx_bytes, stat.tx_errors)
+    #
+    # @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
+    # def _flow_stats_reply_handler(self, ev):
+    #     body = ev.msg.body
+    #
+    #     self.logger.info('port stats : dp %s', ev.msg.datapath.id)
+    #
+    #     self.logger.info('datapath         '
+    #                      'in-port  eth-dst           '
+    #                      'out-port packets  bytes')
+    #     self.logger.info('---------------- '
+    #                      '-------- ----------------- '
+    #                      '-------- -------- --------')
+    #     for stat in sorted([flow for flow in body if flow.priority == 1],
+    #                        key=lambda flow: (flow.match['in_port'],
+    #                                          flow.match['eth_dst'])):
+    #         self.logger.info('%016x %8x %17s %8x %8d %8d',
+    #                          ev.msg.datapath.id,
+    #                          stat.match['in_port'], stat.match['eth_dst'],
+    #                          stat.instructions[0].actions[0].port,
+    #                          stat.packet_count, stat.byte_count)
