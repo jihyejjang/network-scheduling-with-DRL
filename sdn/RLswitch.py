b634df2616bfaf9b70f8676fe36908e91e7a5413
류지혜 <jihye@Rjh-MacBook-Air.local>
2021-08-17 16:46:42 +0900

    0817 commit1

--- sdn/RLswitch.py
+++ sdn/RLswitch.py
@@ -202,13 +202,16 @@ class rl_switch(app_manager.RyuApp):
             out_port = ofproto.OFPP_FLOOD
         #print("out_port",out_port)
         actions = [parser.OFPActionOutput(out_port)]
-        if class_== 1 :
-            match = parser.OFPMatch(in_port=in_port, eth_dst=self.H[5],
-                                    eth_type=0x05dc)
-            #                         eth_type=0x05dc)
-            self.add_flow(datapath, 1000, match, actions)
-            # # verify if we have a valid buffer_id, if yes avoid to send both
-            # # flow_mod & packet_out
+
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            self.add_flow(datapath, 1, match, actions)
+            if class_ == 1:
+                match = parser.OFPMatch(in_port=in_port, eth_dst=self.H[5], eth_type=0x05dc)
+                print("add flow entry")
+                self.add_flow(datapath, 1000, match, actions)
+                # # verify if we have a valid buffer_id, if yes avoid to send both
+                # # flow_mod & packet_out
         if msg.buffer_id != ofproto.OFP_NO_BUFFER:
             self.add_flow(datapath, 1000, match, actions)
             return
