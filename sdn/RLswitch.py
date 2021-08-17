7fcbb416129ac93d30b1b90212187e1d612af8eb
류지혜 <jihye@Rjh-MacBook-Air.local>
2021-08-17 16:39:33 +0900

    0817 commit1

--- sdn/RLswitch.py
+++ sdn/RLswitch.py
@@ -202,19 +202,16 @@ class rl_switch(app_manager.RyuApp):
             out_port = ofproto.OFPP_FLOOD
         #print("out_port",out_port)
         actions = [parser.OFPActionOutput(out_port)]
-        if out_port != ofproto.OFPP_FLOOD:
+        if class_== 1 :
             match = parser.OFPMatch(in_port=in_port, eth_dst=self.H[5],
                                     eth_type=0x05dc)
-            #print("match",msg.match)
-            # match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
-            # match = parser.OFPMatch(in_port=in_port,
             #                         eth_type=0x05dc)
             self.add_flow(datapath, 1000, match, actions)
             # # verify if we have a valid buffer_id, if yes avoid to send both
             # # flow_mod & packet_out
-            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                self.add_flow(datapath, 1000, match, actions)
-                return
+        if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+            self.add_flow(datapath, 1000, match, actions)
+            return
 
         # while True:
         #     try:
@@ -275,13 +272,13 @@ class rl_switch(app_manager.RyuApp):
 
         while True:
             self.cc_cnt += 1
-            match = parser.OFPMatch(in_port=2, eth_type=0x05dc)
+            match = parser.OFPMatch(in_port=2, eth_type=0x05dc, eth_dst=self.H[5])
             actions = [parser.OFPActionOutput(3)]
             self.add_flow(datapath, 1000, match, actions)
             # match = parser.OFPMatch(in_port=2)
             # data = str(time.time()).encode()
-            eth = pkt.get_protocols(ethernet.ethernet)[0]
-            eth.serialize(payload=[str(time.time())],prev=None)
+            # eth = pkt.get_protocols(ethernet.ethernet)[0]
+            # eth.serialize(payload=[str(time.time())],prev=None)
             # pkt.serialize(payload=time.time())
             pkt.serialize()
             out = parser.OFPPacketOut(datapath=datapath,
