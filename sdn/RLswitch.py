6c2b2cf03d0c3fd0c2f3df0b655a5be6798ac9e7
류지혜 <jihye@Rjh-MacBook-Air.local>
2021-09-01 23:31:21 +0900

    0901 commit1

--- sdn/RLswitch.py
+++ sdn/RLswitch.py
@@ -431,23 +431,22 @@ class rl_switch(app_manager.RyuApp):
         class_ = 4 #best effort
         #print("dst",dst)
         match = parser.OFPMatch(in_port = in_port, eth_type=eth_type_)
-
         if (dst in self.H) and (src in self.H):
             #print("dd")
             if eth_type_ == ether_types.ETH_TYPE_IEEE802_3:
-                match = parser.OFPMatch(eth_type=0x05dc)
+                match = parser.OFPMatch(in_port = in_port, eth_type=0x05dc)
                 class_ = 1
                 #print("class_1, inport",in_port)
                 # type_ = 0x05dc
                 #self.logger.info("class %s packet" % (class_))
             elif eth_type_ == ether_types.ETH_TYPE_8021AD:
-                match = parser.OFPMatch(eth_type=0x88a8)
+                match = parser.OFPMatch(in_port = in_port, eth_type=0x88a8)
                 class_ = 2
                 #print("class_2,inport",in_port)
                 # type_ = 0x88a8
                 #self.logger.info("class %s packet" % (class_))
             elif eth_type_ == ether_types.ETH_TYPE_8021AH:
-                match = parser.OFPMatch(eth_type=0x88e7)
+                match = parser.OFPMatch(in_port = in_port, eth_type=0x88e7)
                 class_ = 3
                 #print("class_3,inport",in_port)
                 # type_ = 0x88e7
