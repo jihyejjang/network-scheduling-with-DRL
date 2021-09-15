d5de79bdd402532295f4a16fcfe08ae4993e752c
류지혜 <jihye@Rjh-MacBook-Air.local>
2021-09-15 18:55:53 +0900

    0914 commit1

--- sdn/RLswitch_meter.py
+++ sdn/RLswitch_meter.py
@@ -48,7 +48,7 @@ class rl_switch(app_manager.RyuApp):
         self.timeslot_size = 0.5 #ms
         self.cycle = 10
         self.ts_cnt=0
-        self.gcl = {1: ['0000000000', '0000000000', '0000000000', '1000000000'],
+        self.gcl = {1: ['0000000000', '0000000000', '0000000000', '0000000000'],
                     2: ['0000000000', '0000000000', '0000000000', '1111111111'],
                     3: ['0000000000', '0000000000', '0000000000', '1111111111'],
                     4: ['0000000000', '0000000000', '0000000000', '1111111111'],
@@ -110,13 +110,13 @@ class rl_switch(app_manager.RyuApp):
             self.action_1 = hub.spawn(self.gcl_3)
             self.action_2 = hub.spawn(self.gcl_4)
             self.action_3 = hub.spawn(self.gcl_5)
-            # self.action_4 = hub.spawn(self.gcl_6)
+            self.action_4 = hub.spawn(self.gcl_6)
             self.cc_thread = hub.spawn(self._cc_gen1)
-            # self.cc_thread2 = hub.spawn(self._cc_gen2)
+            self.cc_thread2 = hub.spawn(self._cc_gen2)
             self.ad_thread = hub.spawn(self._ad_gen1)
-            # self.ad_thread2 = hub.spawn(self._ad_gen2)
-            # self.vd_thread =  hub.spawn(self._vd_gen1)
-            # self.vd_thread2 = hub.spawn(self._vd_gen2)
+            self.ad_thread2 = hub.spawn(self._ad_gen2)
+            self.vd_thread =  hub.spawn(self._vd_gen1)
+            self.vd_thread2 = hub.spawn(self._vd_gen2)
 
     #TODO: GCL 적용하기
     def timeslot(self, time):  # timeslot 진행 횟수를 알려주는 함수
@@ -129,13 +129,10 @@ class rl_switch(app_manager.RyuApp):
     def gcl_3(self):
         hub.sleep(3.05)
         datapath = self.dp[3]
-        parser = datapath.ofproto_parser
         gate = np.array([1,1,1,1])
-        match1 = parser.OFPMatch(in_port=1, eth_dst=self.H[4])
-        match2 = parser.OFPMatch(in_port=1, eth_dst=self.H[5])
+
         while True:
             old_gate = gate
-            #goto = parser.OFPInstructionGotoTable(1)
             _,clk = self.timeslot(time.time()) #TODO : clock을 컴퓨터시간에 맞추면 GCl을 순서대로 참조하지 못할 수도 있음
             gate = np.array(list(map(int,self.gcl_[datapath.id][:,clk])))
             change = old_gate - gate
@@ -143,25 +140,23 @@ class rl_switch(app_manager.RyuApp):
             #class 1
             if gate[0] == 0 :
                 self._add_meter(datapath, 1, 0)
-                print("set 0 kbps rate for class 1")
+                #print("set 0 kbps rate for class 1")
             else :
                 self._add_meter(datapath, 1, 1024)
 
             # class 2
             if gate[1] == 0:
                 self._add_meter(datapath, 2, 0)
-                print("set 0 kbps rate for class 2")
+                #print("set 0 kbps rate for class 2")
             else :
                 self._add_meter(datapath, 2, 1024)
 
             # class 3
-            # match3 = parser.OFPMatch()
-            # if gate[2] == '0':
-            #     action3 = parser.OFPInstructionGotoTable(2)
-            # else :
-            #     action3 = goto
-            # #print(action3)
-            # self.add_flow(datapath, 1000, match3, 0, [action3])
+            if gate[1] == 0:
+                self._add_meter(datapath, 3, 0)
+                # print("set 0 kbps rate for class 2")
+            else:
+                self._add_meter(datapath, 3, 1024)
 
             #TODO : class 4
 
@@ -173,8 +168,6 @@ class rl_switch(app_manager.RyuApp):
     def gcl_4(self):
         hub.sleep(3.05)
         datapath = self.dp[4]
-        #ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
         gate = np.array([1,1,1,1])
         while True:
             old_gate = gate
@@ -185,16 +178,23 @@ class rl_switch(app_manager.RyuApp):
             # class 1
             if gate[0] == 0:
                 self._add_meter(datapath, 1, 0)
-                print("set 0 kbps rate for class 1")
+                #print("set 0 kbps rate for class 1")
             else:
                 self._add_meter(datapath, 1, 1024)
 
             # class 2
             if gate[1] == 0:
                 self._add_meter(datapath, 2, 0)
-                print("set 0 kbps rate for class 2")
+                #print("set 0 kbps rate for class 2")
             else:
                 self._add_meter(datapath, 2, 1024)
+
+            # class 3
+            if gate[1] == 0:
+                self._add_meter(datapath, 3, 0)
+                # print("set 0 kbps rate for class 2")
+            else:
+                self._add_meter(datapath, 3, 1024)
 
             #TODO : class 4
 
@@ -203,8 +203,6 @@ class rl_switch(app_manager.RyuApp):
     def gcl_5(self):
         hub.sleep(3.05)
         datapath = self.dp[5]
-        #ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
         gate = np.array([1,1,1,1])
         while True:
             old_gate = gate
@@ -215,62 +213,62 @@ class rl_switch(app_manager.RyuApp):
             # class 1
             if gate[0] == 0:
                 self._add_meter(datapath, 1, 0)
-                print("set 0 kbps rate for class 1")
+                #print("set 0 kbps rate for class 1")
             else:
                 self._add_meter(datapath, 1, 1024)
 
             # class 2
             if gate[1] == 0:
                 self._add_meter(datapath, 2, 0)
-                print("set 0 kbps rate for class 2")
+                #print("set 0 kbps rate for class 2")
             else:
                 self._add_meter(datapath, 2, 1024)
+
+            # class 3
+            if gate[1] == 0:
+                self._add_meter(datapath, 3, 0)
+                # print("set 0 kbps rate for class 2")
+            else:
+                self._add_meter(datapath, 3, 1024)
 
             #TODO : class 4
 
             hub.sleep(0.0004)
 
-    # def gcl_6(self):
-    #     hub.sleep(3.05)
-    #     datapath = self.dp[6]
-    #     #ofproto = datapath.ofproto
-    #     parser = datapath.ofproto_parser
-    #     while True:
-    #         goto = parser.OFPInstructionGotoTable(1)
-    #         _,clk = self.timeslot(time.time())
-    #         gate=self.gcl_[datapath.id][:,clk]
-    #         # print("gate:",gate)
-    #
-    #         #class 1
-    #         match1 = parser.OFPMatch(eth_type=0x05dc)
-    #         if gate[0] == '0' :
-    #             action1 = parser.OFPInstructionGotoTable(2)
-    #         else :
-    #             action1 = goto
-    #         #print(action1)
-    #         self.add_flow_(datapath, 1000, match1, 0, [action1])
-    #
-    #         # class 2
-    #         match2 = parser.OFPMatch(eth_type=0x88a8)
-    #         if gate[1] == '0':
-    #             action2 = parser.OFPInstructionGotoTable(2)
-    #         else :
-    #             action2 = goto
-    #         #print(action2)
-    #         self.add_flow_(datapath, 1000, match2, 0, [action2])
-    #
-    #         # class 3
-    #         match3 = parser.OFPMatch(eth_type=0x88e7)
-    #         if gate[2] == '0':
-    #             action3 = parser.OFPInstructionGotoTable(2)
-    #         else :
-    #             action3 = goto
-    #         #print(action3)
-    #         self.add_flow_(datapath, 1000, match3, 0, [action3])
-    #
-    #         #TODO : class 4
-    #
-    #         hub.sleep(0.0004)
+    def gcl_6(self):
+        hub.sleep(3.05)
+        datapath = self.dp[6]
+        gate = np.array([1, 1, 1, 1])
+        while True:
+            old_gate = gate
+            _, clk = self.timeslot(time.time())  # TODO : clock을 컴퓨터시간에 맞추면 GCl을 순서대로 참조하지 못할 수도 있음
+            gate = np.array(list(map(int, self.gcl_[datapath.id][:, clk])))
+            change = old_gate - gate
+
+            # class 1
+            if gate[0] == 0:
+                self._add_meter(datapath, 1, 0)
+                # print("set 0 kbps rate for class 1")
+            else:
+                self._add_meter(datapath, 1, 1024)
+
+            # class 2
+            if gate[1] == 0:
+                self._add_meter(datapath, 2, 0)
+                # print("set 0 kbps rate for class 2")
+            else:
+                self._add_meter(datapath, 2, 1024)
+
+            # class 3
+            if gate[1] == 0:
+                self._add_meter(datapath, 3, 0)
+                # print("set 0 kbps rate for class 2")
+            else:
+                self._add_meter(datapath, 3, 1024)
+
+            # TODO : class 4
+
+            hub.sleep(0.0004)
 
 
     # def gcl_cycle(self):
@@ -291,14 +289,6 @@ class rl_switch(app_manager.RyuApp):
     #                    format(np.argmax(self.model4.predict(self.state[s].reshape(-1,4))), '010b')]
     #             print(self.gcl[s])
     #
-    @set_ev_cls(ofp_event.EventOFPErrorMsg,
-                [HANDSHAKE_DISPATCHER, CONFIG_DISPATCHER, MAIN_DISPATCHER])
-    def error_msg_handler(self, ev):
-        msg = ev.msg
-
-        self.logger.debug('OFPErrorMsg received: type=0x%02x code=0x%02x ',
-                          msg.type, msg.code)
-
     #flow entry modification message
     def add_flow_(self, datapath, priority, match, tableid, inst):
         ofproto = datapath.ofproto
@@ -317,7 +307,7 @@ class rl_switch(app_manager.RyuApp):
     def add_meter(self, datapath, m_id):
         parser = datapath.ofproto_parser
         ofproto = datapath.ofproto
-        band = parser.OFPMeterBandDrop(rate = 1024, burst_size = 1024) #TODO: drop으로 인해 packet drop이 일어날 수도 있음
+        band = parser.OFPMeterBandDrop(rate = 0, burst_size = 1024) #TODO: drop으로 인해 packet drop이 일어날 수도 있음
         mod = parser.OFPMeterMod(datapath, ofproto.OFPMC_ADD, ofproto.OFPMF_KBPS, m_id, [band])
         datapath.send_msg(mod)
 
@@ -331,142 +321,50 @@ class rl_switch(app_manager.RyuApp):
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         msg = ev.msg
-        #fields = msg.match.fields
-        #print(fields)
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
-
-        # for f in fields:
-        #     if f.header == ofproto.OXM_OF_IN_PORT:
-        #         in_port = f.value
-        #         print ("in_port",in_port)
-        #     elif f.header == ofproto.OXM_OF_ETH_SRC:
-        #         eth_src = f.value
-        #         print("eth_src",eth_src)
-        #     elif f.header == ofproto.OXM_OF_ETH_DST:
-        #         eth_dst = f.value
-        #         print("eth_dst", eth_dst)
-        #     elif f.header == ofproto.OXM_OF_ETH_TYPE:
-        #         et_ty = f.value
-        #         print("eth_type", et_ty)
-
-        #table_id = msg.table_id
-        #fields = msg.match.fields
         switchid = datapath.id
-        #bufferid = msg.buffer_id
-
-        #print ("table_id", table_id)
-        #print ("fields", fields)
-
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
         eth_type_ = eth.ethertype
-        #print("packet in", eth.ethertype)
         dst = eth.dst
-        #print("eth.dst",dst)
         src = eth.src
-        #print("eth.src",src)
-        #print("ofproto.OXM_OF_IN_PORT",ofproto.OXM_OF_IN_PORT)
-        #print("ofproto.OXM_OF_ETH_SRC",ofproto.OXM_OF_ETH_SRC)
-
-        class_ = 4 #best effort
-        #print("dst",dst)
+        class_ = 4
         match = parser.OFPMatch(in_port = in_port, eth_dst=dst)
+
         if (dst in self.H) and (src in self.H):
             #print("dd")
             if eth_type_ == ether_types.ETH_TYPE_IEEE802_3:
-                match1 = parser.OFPMatch(eth_type=0x05dc)
-                #match = parser.OFPMatch(in_port=in_port)
                 class_ = 1
-                #print("class_1, inport",in_port)
-                # type_ = 0x05dc
-                #self.logger.info("class %s packet" % (class_))
             elif eth_type_ == ether_types.ETH_TYPE_8021AD:
-                match1 = parser.OFPMatch(eth_type=0x88a8)
-                #match = parser.OFPMatch(in_port=in_port)
                 class_ = 2
-                #print("class_2,inport",in_port)
-                # type_ = 0x88a8
-                #self.logger.info("class %s packet" % (class_))
             elif eth_type_ == ether_types.ETH_TYPE_8021AH:
-                match1 = parser.OFPMatch(eth_type=0x88e7)
-                #match = parser.OFPMatch(in_port=in_port)
                 class_ = 3
-                #print("class_3,inport",in_port)
-                # type_ = 0x88e7
-                #self.logger.info("class %s packet" % (class_))
-
-        else :
+        else : #best effort
             self.add_flow(datapath, 1, match, 0, [])
             return
 
         if dst in self.mac_to_port[switchid]:
             out_port = self.mac_to_port[switchid][dst]
-            #print("inport %s,out_port %s" % (in_port, out_port))
-            # self.queue[switchid - 1][out_port - 1][class_ - 1] += 1
         else:
             out_port = ofproto.OFPP_FLOOD
 
-        actions1 = [parser.OFPActionOutput(out_port)]
-        actions1.insert(0,parser.OFPActionSetQueue(out_port))
-        #goto = parser.OFPInstructionGotoTable(1) # 1: sending packet to port, 2: queueing packet
-        #actions2 = parser.OFPActionSetQueue(out_port)
-        #actions2 = parser.OFPActionOutput(out_port)
-        inst1 = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions1),
+        actions = [parser.OFPActionOutput(out_port)]
+        actions.insert(0,parser.OFPActionSetQueue(out_port))
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions),
                  parser.OFPInstructionMeter(class_, ofproto.OFPIT_METER)]
-
-        #inst2 = parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, [actions2])
-
-        #self.add_flow(datapath, 100, match, 1, [inst1])
-        #self.add_flow(datapath, 100, match, 2, [inst2])
-        self.add_flow(datapath, 100, match, 0, inst1)
+        self.add_flow(datapath, 100, match, class_, inst)
         self.add_meter(datapath, class_)
 
-        #self.thread.append(hub.spawn(self._gcl_, datapath, match))
+        goto = parser.OFPInstructionGotoTable(class_)
+        self.add_flow(datapath, 100, match, 0, [goto])
 
-        #print("add_flow")
-
-        # if out_port != ofproto.OFPP_FLOOD:
-        #     match = parser.OFPMatch(in_port=in_port, eth_type=type_)
-        #     if class_ != 4 :
-        #         self.add_flow(datapath, 1000, match, actions)
-        #         print("add flow entry")
-        # if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-        #     # self.add_flow(datapath, 1000, match, actions)
-        #     return
-
-        # while True:
-        #     try:
-        # delay = (self.gcl[switchid][class_-1][clk - 1:].index('1')) * self.timeslot_size  # gate가 open되기까지의 시간을 계산 (만약 열려있으면 바로 전송)
-                # break
-            # except:
-            #     print("다음 cycle까지 기다리기 : 현재 사이클에 OPEN예정이 없음")
-            #     time.sleep(self.timeslot_size/1000)
-
-        #print("delay", delay/1000)
-
-        # hub.sleep(delay/1000) #delay
-
-        # match = parser.OFPMatch(in_port = in_port)
-        #flow가 match와 일치하면 match생성시에 지정해준 action으로 packet out한다.
-        # delay_end_time = 0
-        # if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-        #     delay_end_time = time.time()
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  in_port=in_port, actions=actions1, data=pkt.data)
+                                  in_port=in_port, actions=actions, data=pkt.data)
 
         datapath.send_msg(out)
-
-
-        # if (1 <= out_port <= 3):
-        #     self.queue[switchid-1][out_port-1][class_-1] -= 1
-        #     df = pd.DataFrame([(delay_end_time, switchid, class_, '-', delay_end_time - delay_start_time,
-        #                         self.queue[switchid - 1][out_port - 1][class_ - 1])],
-        #                       columns=['arrival time', 'switch', 'class', 'number', 'delay', 'queue'])
-        #     self.received_log = self.received_log.append(df)
-
         if class_ != 4:
             self.logger.info("[in] %f : 스위치 %s, class %s 패킷" % \
                                  (time.time(), switchid, class_))
@@ -673,50 +571,3 @@ class rl_switch(app_manager.RyuApp):
             if (self.vd_cnt2 >= self.video):
                 self.terminal += 1
                 break
-
-
-    #
-    # def _vd_gen2(self):
-    #     datapath = self.dp[2]
-    #     pkt = packet.Packet()
-    #     pkt.add_protocol(ethernet.ethernet(ethertype=ether_types.ETH_TYPE_8021AH,
-    #                                        dst=self.H[7],
-    #                                        src=self.H[3]))
-    #
-    #     ofproto = datapath.ofproto
-    #     parser = datapath.ofproto_parser
-    #     pkt.serialize()
-    #
-    #     match = parser.OFPMatch(in_port=2, eth_dst=self.H[7])
-    #     actions = [parser.OFPActionOutput(3)]
-    #     self.add_flow(datapath, 1, match, actions, ofproto.OFP_NO_BUFFER)
-    #     match = parser.OFPMatch(in_port=2)
-    #     data = pkt.data
-    #
-    #     out = parser.OFPPacketOut(datapath=datapath,
-    #                               buffer_id=ofproto.OFP_NO_BUFFER,
-    #                               match=match,
-    #                               actions=actions, data=data)
-    #     while True:
-    #         self.vd_cnt2 += 1
-    #         datapath.send_msg(out)
-    #         self.logger.info("%f : video2 generated %s, 스위치%s " % (time.time(), self.vd_cnt2, datapath.id))
-    #
-    #         df = pd.DataFrame([(datapath.id, 3, self.vd_cnt2, time.time(), 'x')],
-    #                           columns=['switch', 'class', 'number', 'time', 'queue'])
-    #         self.generated_log = self.generated_log.append(df)
-    #         hub.sleep(self.vd_period / 1000)
-    #         if (self.vd_cnt2 >= self.video):
-    #             self.terminal+=1
-    #             break
-                # if (self.cc_cnt >= self.command_control) and (self.cc_cnt2 >= self.command_control) and (self.ad_cnt >= self.audio) \
-                #         and (self.ad_cnt2 >= self.audio) and (self.vd_cnt >= self.video):
-                #     self.terminal=True
-                #     break
-
-            #
-            # if (self.cc_cnt >= self.command_control) and (self.cc_cnt2 >= self.command_control) and (
-            #         self.ad_cnt >= self.audio) \
-            #         and (self.ad_cnt2 >= self.audio) and (self.vd_cnt >= self.video) and (self.vd_cnt2 >= self.video):
-            #     self.terminal = True
-            #     break
